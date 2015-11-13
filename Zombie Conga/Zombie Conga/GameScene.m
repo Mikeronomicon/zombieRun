04c301dcc7696d2867ae40d75aa27111fa1bdf99
Michael <Mikeronomicon@gmail.com>
2015-11-12 16:05:32 -0800

    got background to display properly

--- Zombie Conga/Zombie Conga/GameScene.m
+++ Zombie Conga/Zombie Conga/GameScene.m
@@ -15,7 +15,13 @@
     if (self = [super initWithSize:size]) {
         self.backgroundColor = [SKColor whiteColor];
         SKSpriteNode *bg = [SKSpriteNode spriteNodeWithImageNamed:@"background"];
+        bg.position = CGPointMake(self.size.width/2, self.size.height/2);
+        bg.position = CGPointMake(self.size.width / 2, self.size.height / 2);
+        bg.anchorPoint = CGPointMake(0.5, 0.5); // same as default
+        //bg.zRotation = M_PI / 8;
         [self addChild:bg];
+        CGSize mySize = bg.size;
+        NSLog(@"Size: %@", NSStringFromCGSize(mySize));
     }
     return self;
 }
--- Zombie Conga/Zombie Conga/GameViewController.m
+++ Zombie Conga/Zombie Conga/GameViewController.m
@@ -9,45 +9,24 @@
 #import "GameViewController.h"
 #import "GameScene.h"
 
-@implementation SKScene (Unarchive)
-
-+ (instancetype)unarchiveFromFile:(NSString *)file {
-    /* Retrieve scene file path from the application bundle */
-    NSString *nodePath = [[NSBundle mainBundle] pathForResource:file ofType:@"sks"];
-    /* Unarchive the file to an SKScene object */
-    NSData *data = [NSData dataWithContentsOfFile:nodePath
-                                          options:NSDataReadingMappedIfSafe
-                                            error:nil];
-    NSKeyedUnarchiver *arch = [[NSKeyedUnarchiver alloc] initForReadingWithData:data];
-    [arch setClass:self forClassName:@"SKScene"];
-    SKScene *scene = [arch decodeObjectForKey:NSKeyedArchiveRootObjectKey];
-    [arch finishDecoding];
-    
-    return scene;
-}
-
-@end
-
 @implementation GameViewController
 
 - (void)viewWillLayoutSubviews
 {
     [super viewWillLayoutSubviews];
-
+    
     // Configure the view.
     SKView * skView = (SKView *)self.view;
     if (!skView.scene) {
-    skView.showsFPS = YES;
-    skView.showsNodeCount = YES;
-    /* Sprite Kit applies additional optimizations to improve rendering performance */
-    skView.ignoresSiblingOrder = YES;
-    
-    // Create and configure the scene.
-    GameScene *scene = [GameScene unarchiveFromFile:@"GameScene"];
-    scene.scaleMode = SKSceneScaleModeAspectFill;
-    
-    // Present the scene.
-    [skView presentScene:scene];
+        skView.showsFPS = YES;
+        skView.showsNodeCount = YES;
+        
+        // Create and configure the scene.
+        SKScene * scene = [GameScene sceneWithSize:skView.bounds.size];
+        scene.scaleMode = SKSceneScaleModeAspectFill;
+        
+        // Present the scene.
+        [skView presentScene:scene];
     }
 }
 
@@ -56,14 +35,14 @@
     return YES;
 }
 
-- (UIInterfaceOrientationMask)supportedInterfaceOrientations
-{
-    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
-        return UIInterfaceOrientationMaskAllButUpsideDown;
-    } else {
-        return UIInterfaceOrientationMaskAll;
-    }
-}
+//- (NSUInteger)supportedInterfaceOrientations
+//{
+//    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
+//        return UIInterfaceOrientationMaskAllButUpsideDown;
+//    } else {
+//        return UIInterfaceOrientationMaskAll;
+//    }
+//}
 
 - (void)didReceiveMemoryWarning
 {
@@ -71,8 +50,10 @@
     // Release any cached data, images, etc that aren't in use.
 }
 
-- (BOOL)prefersStatusBarHidden {
+-(BOOL)prefersStatusBarHidden
+{
     return YES;
 }
 
 @end
+
